9ddec7287e60592ef029291247cf47df7266a623
Marek Šoška <mareksoska22@gmail.com>
2021-11-13 16:32:55 +0100

    Remove vscode files

--- .devcontainer.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-source /opt/netbox/venv/bin/activate &&
-python3 setup.py develop &&
-sed -i "s|^PLUGINS = \[\(\s*\S+\s*\)\]|PLUGINS = \[\1, \"netbox_netdisco\"\]|g" /opt/netbox/netbox/netbox/configuration.py
-sed -i "s|^PLUGINS = \[\s*\]|PLUGINS = \[\"netbox_netdisco\"\]|g" /opt/netbox/netbox/netbox/configuration.py
