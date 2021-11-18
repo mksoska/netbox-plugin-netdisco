e2ac8776204f781534853da85a5b7ab5a7c7a659
Marek Šoška <mareksoska22@gmail.com>
2021-11-18 22:07:42 +0100

    Create device inventory

---
+++ netbox_netdisco/core/inventory.py
@@ -0,0 +1,32 @@
+from .models import *
+from .netdisco import Netdisco
+
+class Inventory():
+    # Later add support for AsyncResult (maybe even HTTPResponse) returned from any get method of models
+
+    @staticmethod
+    def collect(**kwargs):
+        Device.objects.clear()
+        Port.objects.clear()
+        Address.objects.clear()
+        Vlan.objects.clear()
+
+        Netdisco.start_session()
+        Inventory._initialize_devices(**kwargs)
+        Netdisco.close()
+
+    #TODO: Make prettier (at least naming)
+    @staticmethod
+    def _initialize_devices(**kwargs): 
+        for portutilization in Netdisco.reports.api_v1_report_device_portutilization_get(**kwargs):
+            device = Device._get(portutilization.ip, **kwargs)
+            if Device._is_apimodel(device):
+                Device(device, **kwargs)
+
+    #TODO: Find inconsistencies in inventory
+    def compute():
+        pass
+
+    #TODO: Send inconsistencies notification to Icinga
+    def notify():
+        pass
