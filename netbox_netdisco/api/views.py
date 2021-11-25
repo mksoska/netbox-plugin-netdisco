5f31b2ab036249a15ad0fa46ddf97f5419c95394
Marek Šoška <mareksoska22@gmail.com>
2021-11-25 01:25:34 +0100

    Change collect api endpoint response

--- netbox_netdisco/api/views.py
+++ netbox_netdisco/api/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework import permissions
 from ..core import Inventory
+import json
 
 @api_view(['POST'])
 @permission_classes((permissions.AllowAny,))
@@ -9,8 +10,13 @@ def inventory_collect(request):
     """Trigger Netdisco inventory data retrieval."""
     Inventory.collect()
     response = Inventory.notify()
+    
     return HttpResponse(
-        content=response.content,
-        status=response.status_code,
+        content=json.dumps({"collect": "Inventory collected.", "notify": "Notification successfuly sent."}),
+        status=200,
+        content_type=response.headers['Content-Type']
+    ) if response.status_code == 200 else HttpResponse(
+        content=json.dumps({ "collect": "Inventory collected.", "notify": "Notification failed."}),
+        status=500,
         content_type=response.headers['Content-Type']
     )
