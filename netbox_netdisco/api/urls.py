c5b35f842f359d44a0a268dbe9dbbf101a8ce5b3
Marek Šoška <mareksoska22@gmail.com>
2021-11-26 17:39:41 +0100

    Add inventory API endpoints

--- netbox_netdisco/api/urls.py
+++ netbox_netdisco/api/urls.py
@@ -2,5 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('collect/', views.inventory_collect, name='inventory_collect')
+    path('collect/', views.inventory_collect, name='inventory_collect'),
+    path('notify/', views.inventory_notify, name='inventory_notify'),
+
+    path('collect/device/', views.collect_devices, name='collect_devices'),
+    path('collect/device/<ip>/', views.collect_device, name='collect_device'),
+
+    path('collect/device/<ip>/port/', views.collect_ports, name='collect_ports'),
+    path('collect/device/<ip>/port/<port>/', views.collect_port, name='collect_port'),
+
+    path('collect/device/<ip>/address/', views.collect_addresses, name='collect_addresses'),
+    path('collect/device/<ip>/vlan/', views.collect_vlans, name='collect_vlans')
 ]
--- netbox_netdisco/api/views.py
+++ netbox_netdisco/api/views.py
@@ -4,19 +4,75 @@ from rest_framework import permissions
 from ..core import Inventory
 import json
 
+
+def collect_response(label):
+    return HttpResponse(
+        content=json.dumps({"collect": label + "collected."}),
+        status=200,
+        content_type='Accept: application/json'
+    )
+
+
 @api_view(['POST'])
 @permission_classes((permissions.AllowAny,))
 def inventory_collect(request):
     """Trigger Netdisco inventory data retrieval."""
     Inventory.collect()
+    return collect_response("Inventory ")
+    
+@api_view(['POST'])
+@permission_classes((permissions.AllowAny,))
+def inventory_notify(request):
     response = Inventory.notify()
     
     return HttpResponse(
-        content=json.dumps({"collect": "Inventory collected.", "notify": "Notification successfuly sent."}),
+        content=json.dumps({"notify": "Notification successfuly sent."}),
         status=200,
         content_type=response.headers['Content-Type']
     ) if response.status_code == 200 else HttpResponse(
-        content=json.dumps({ "collect": "Inventory collected.", "notify": "Notification failed."}),
+        content=json.dumps({"notify": "Notification failed."}),
         status=500,
         content_type=response.headers['Content-Type']
     )
+
+
+@api_view(['POST'])
+@permission_classes((permissions.AllowAny,))
+def collect_devices(request):
+    Inventory.collect_devices()
+    return collect_response("Devices ")
+
+
+@api_view(['POST'])
+@permission_classes((permissions.AllowAny,))
+def collect_device(request, ip):
+    Inventory.collect_device(ip)
+    return collect_response(f"Device {ip} ")
+
+@api_view(['POST'])
+@permission_classes((permissions.AllowAny,))
+def collect_ports(request, ip):
+    Inventory.collect_ports(ip)
+    return collect_response("Ports of device {ip} ")
+
+
+@api_view(['POST'])
+@permission_classes((permissions.AllowAny,))
+def collect_port(request, ip, port):
+    Inventory.collect_port(ip, port)
+    return collect_response("Port {port} of device {ip} ")
+
+
+@api_view(['POST'])
+@permission_classes((permissions.AllowAny,))
+def collect_addresses(request, ip):
+    Inventory.collect_addresses(ip)
+    return collect_response("IP addresses of device {ip} ")
+
+
+@api_view(['POST'])
+@permission_classes((permissions.AllowAny,))
+def collect_vlans(request, ip):
+    Inventory.collect_vlans(ip)
+    return collect_response("VLANs of device {ip} ")
+
