73c87c2cfe889f0b62b67aec363a9c9fede1f575
Marek Šoška <mareksoska22@gmail.com>
2021-12-10 23:06:39 +0100

    Remove unused commented code

--- netbox_netdisco/api/urls.py
+++ netbox_netdisco/api/urls.py
@@ -6,17 +6,4 @@ urlpatterns = [
     path('inventory/notify/', views.inventory_notify, name='inventory_notify'),
 
     path('device/collect/', views.device_collect, name='device_collect')
-
-    # path('collect/device/', views.collect_devices, name='collect_devices'),
-    # path('collect/device/<ip>/', views.collect_device, name='collect_device'),
-
-    # path('collect/port/', views.collect_ports, name='collect_ports'),
-    # path('collect/device/<ip>/port/', views.collect_device_ports, name='collect_device_ports'),
-    # path('collect/device/<ip>/port/<port>/', views.collect_port, name='collect_port'),
-
-    # path('collect/address/', views.collect_addresses, name='collect_addresses'),
-    # path('collect/device/<ip>/address/', views.collect_device_addresses, name='collect_device_addresses'),
-
-    # path('collect/vlan/', views.collect_vlans, name='collect_vlans'),
-    # path('collect/device/<ip>/vlan/', views.collect_device_vlans, name='collect_device_vlans')
 ]
